a56eb8d1d59c639191e9ed10cd48000ccab30fad
rahul-soshte <rahul.soshte47@gmail.com>
2016-12-13 00:32:36 +0530

    Starbuzz practice app

---
+++ app/src/main/java/com/example/hunter/starbuzz/TopLevelActivity.java
@@ -0,0 +1,36 @@
+package com.example.hunter.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.AdapterView;
+
+import android.widget.ListView;
+import android.view.View;
+
+
+ public class TopLevelActivity extends Activity {
+  @Override
+     protected void onCreate(Bundle savedInstanceState)
+  {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_top_level);
+//Create an OnItemClickListener
+
+      AdapterView.OnItemClickListener itemClickListener=new AdapterView.OnItemClickListener(){
+          @Override
+          public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+              if(position==0)
+              {
+                  Intent intent=new Intent(TopLevelActivity.this,DrinkCategoryActivity.class);
+                  startActivity(intent);
+
+              }
+          }
+      };
+      //Add the listener to the list View
+      ListView listView=(ListView)findViewById(R.id.list_options);
+      listView.setOnItemClickListener(itemClickListener);
+  }
+
+ }
